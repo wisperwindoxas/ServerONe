3f70babb676483353f79382232f816ef28002d4d
wisperwindoxas <windoxas@gmail.com>
2021-09-17 05:13:02 +0500

    ger

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 let last = document.querySelector(".last")
 let first = document.querySelector(".first")
+let send = document.querySelector(".send")
+
+const data = {LastName:last.value, FirstName:first.value, age:45}
 
 
-fetch(\)
+send.addEventListener('click', (e) =>{
+    console.log(data);
+    e.preventDefault()
+    fetch('https://my-json-server.typicode.com/wisperwindoxas/ServerOne/Kumkurgan',{
+    method: 'POST', // или 'PUT'
+    body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
+    headers: {
+      'Content-Type': 'application/json'
+    }
+})
+})
